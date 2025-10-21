3f89437915a365240cf0c312967cc41bc9454a49
Sergey-Draft <draftkrot1987@gmail.com>
2025-10-21 18:14:50 +0300

    4345353

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import "./App.css";
-import Sidebar from "./Sidebar";
+import Sidebar from "./Sidebar2342";
 import CreditForm from "./CreditForm";
 import BankCard from "./BankCredit";
 
